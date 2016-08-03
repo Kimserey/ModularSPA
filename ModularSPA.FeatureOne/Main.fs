e545d0f4cba806d80953719867e63098394eff70
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-08-03 13:44:38 +0100

    firt commit

--- ModularSPA.FeatureOne/Main.fs
+++ ModularSPA.FeatureOne/Main.fs
@@ -1,3 +1,11 @@
-namespace ModularSPA.FeatureOne
+namespace ModularSPA
 
 open WebSharper
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Html
+
+[<JavaScript>]
+module FeatureOne =
+
+    let doc =
+        h1 [ text "This is feature ONE" ]
--- ModularSPA.FeatureTwo/Main.fs
+++ ModularSPA.FeatureTwo/Main.fs
@@ -1,3 +1,11 @@
-namespace ModularSPA.FeatureTwo
+namespace ModularSPA
 
 open WebSharper
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Html
+
+[<JavaScript>]
+module FeatureTwo =
+
+    let doc =
+        h1 [ text "This is feature TWO" ]
